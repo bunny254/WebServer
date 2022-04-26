1f3a40cb5efbc5d000c5dfd20e37ee3bdc0fb66b
bunny254 <kimaniwachira03@gmail.com>
2022-04-26 09:20:17 +0300

    template literal

--- app2.js
+++ app2.js
@@ -1,9 +1,10 @@
 var http= require('http');
+var port= 3000;
 
-var server=http.createServer(function(request,response){
+var server=http.createServer (function(request,response){
 console.log('Incoming Request!');
 response.write('Hello');
 response.end();
 });
 
-server.listen(3000);
+server.listen(`${port}`);
